3a415d166dbc8a01877ed1130283263570589c5b
obavanteshia <obavanteshia65@gmail.com>
2026-01-13 16:09:01 +0300

    fix: correct import path for api in Notifications.jsx for Vercel build

--- src/pages/components/Notifications.jsx
+++ src/pages/components/Notifications.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import api from "../../api/api"; // <-- fixed path
 
+import api from "../../api/api.js";
 import { useNavigate } from "react-router-dom";
 
 export default function Notifications() {
