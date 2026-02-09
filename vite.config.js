4e4c51f71a73c86e0da45263a78bdd2cbda30235
obavanteshia <obavanteshia65@gmail.com>
2026-02-09 21:03:14 +0300

    Fix MIME type headers for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react"; 
 export default defineConfig({ 
   plugins: [react()], 
-  build: { outDir: "dist" }, 
+  build: { 
+    outDir: "dist", 
+    rollupOptions: { 
+      output: { 
+        entryFileNames: "assets/[name].js", 
+        chunkFileNames: "assets/[name].js", 
+        assetFileNames: "assets/[name].[ext]" 
+      } 
+    } 
+  }, 
   base: "/", 
 }); 
