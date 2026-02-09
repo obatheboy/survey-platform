7cf98fbe5084b997c05c3e6b05c9e8a44aaf2611
obavanteshia <obavanteshia65@gmail.com>
2026-02-09 20:35:25 +0300

    Fix asset loading

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    }
-  },
-  server: {
-    port: 5173,
-  },
-  base: "./",
-});
+import { defineConfig } from "vite"; 
+import react from "@vitejs/plugin-react"; 
+export default defineConfig({ 
+  plugins: [react()], 
+  build: { outDir: "dist" }, 
+  base: "/", 
+}); 
