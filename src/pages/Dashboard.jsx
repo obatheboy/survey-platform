0efc0bbf8abebe1e6372aace3014ad85de6c0b1b
obavanteshia <obavanteshia65@gmail.com>
2026-02-26 08:32:48 +0300

    Fix: res

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1968,6 +1968,7 @@ return (
         isOpen={showWelcomeBonus}
         onClose={handleWelcomeBonusClose}
         bonusAmount={welcomeBonusAmount}
+        onActivate={() => navigate('/withdraw-form')}
       />
 
       {/* DAILY REWARD POPUP */}
