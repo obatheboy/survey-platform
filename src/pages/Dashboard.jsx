2d6a04729c73f41a1b03c80eeee2ddbe86f0e08c
obavanteshia <obavanteshia65@gmail.com>
2026-03-02 02:05:38 +0300

    UI impro

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -904,7 +904,7 @@ return (
         borderBottom: '1px solid rgba(255,255,255,0.2)',
         position: 'relative',
         overflow: 'hidden',
-        marginTop: '90px',
+        marginTop: '12px',
         marginBottom: '0'
       }}>
         <div style={{
