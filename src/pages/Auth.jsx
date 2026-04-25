bc45fa5b8bef99f6844358e18652e3ac684dbda3
obavanteshia <obavanteshia65@gmail.com>
2026-04-25 11:15:58 +0300

    fix:

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -402,7 +402,7 @@ export default function Auth() {
           style={styles.supportBtn}
           onClick={() => {
             const message = encodeURIComponent("Hello, I need help with creating my survey account.");
-            window.open(`https://wa.me/254140834185?text=${message}`, "_blank");
+            window.open(`https://wa.me/254785619533?text=${message}`, "_blank");
           }}
         >
           💬 Chat Support
